3564e8c88fa9ccc724eb91b2d156695dc4637afa
Puneet Kant <puneetkantx250@gmail.com>
2019-12-31 23:25:09 +0530

    initial commit of superslider apps

---
+++ SuperSlider/SuperSlider/SuperSlider/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using SuperSlider.Models;
+
+namespace SuperSlider.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
