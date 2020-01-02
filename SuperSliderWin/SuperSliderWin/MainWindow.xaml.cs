406b5dc18ed3319b610c4c0f5940f704294ac259
Puneet Kant <puneetkantx250@gmail.com>
2020-01-03 02:20:59 +0530

    trying to fix memory leak and size issue

--- SuperSliderWin/SuperSliderWin/MainWindow.xaml.cs
+++ SuperSliderWin/SuperSliderWin/MainWindow.xaml.cs
@@ -19,6 +19,7 @@ namespace SuperSliderWin
         private static bool _updateFirstImage = true;
         private static DoubleAnimation _fadeInAnimation;
         private static DoubleAnimation _fadeOutAnimation;
+        private static FileStream _fileStream;
 
         public Settings Settings { get; set; }
         public MainWindow()
@@ -36,8 +37,8 @@ namespace SuperSliderWin
         {
             SlideImage.Stretch = this.Settings.Style;
             SlideImage.StretchDirection = StretchDirection.Both;
-            SlideImageSecond.Stretch = this.Settings.Style;
-            SlideImageSecond.StretchDirection = StretchDirection.Both;
+            //SlideImageSecond.Stretch = this.Settings.Style;
+            //SlideImageSecond.StretchDirection = StretchDirection.Both;
             var fadeInTime = new TimeSpan(0, 0, 0, 0, (int)(this.Settings.FadeInTime * 1000));
             var fadeOutTime = new TimeSpan(0, 0, 0, 0, (int)(this.Settings.FadeOutTime * 1000));
             _fadeInAnimation = new DoubleAnimation(1d, fadeInTime);
@@ -59,52 +60,72 @@ namespace SuperSliderWin
 
                 int nextRandom = _random.Next(0, _imageFiles.Length - 1);
                 var imageName = _imageFiles[nextRandom];
-                ImageNameTextBlock.Text = imageName;
+                //ImageNameTextBlock.Text = imageName;
 
-                double snugContentWidth = this.ActualWidth;
-                double snugContentHeight = this.ActualHeight;
+                //double snugContentWidth = this.ActualWidth;
+                //double snugContentHeight = this.ActualHeight;
 
-                var horizontalBorderHeight = SystemParameters.ResizeFrameHorizontalBorderHeight;
-                var verticalBorderWidth = SystemParameters.ResizeFrameVerticalBorderWidth;
-                var captionHeight = SystemParameters.CaptionHeight;
+                //var horizontalBorderHeight = SystemParameters.ResizeFrameHorizontalBorderHeight;
+                //var verticalBorderWidth = SystemParameters.ResizeFrameVerticalBorderWidth;
+                //var captionHeight = SystemParameters.CaptionHeight;
 
-                var clientWidth = snugContentWidth + 2.0 * verticalBorderWidth;
-                var clientHeight = snugContentHeight + captionHeight + 2.0 * horizontalBorderHeight;
+                //var clientWidth = snugContentWidth + 2.0 * verticalBorderWidth;
+                //var clientHeight = snugContentHeight + captionHeight + 2.0 * horizontalBorderHeight;
 
                 BitmapImage bitImage = new BitmapImage();
-                bitImage.CacheOption = BitmapCacheOption.None;
+                if (_fileStream != null)
+                {
+                    _fileStream.Close();
+                    _fileStream.Dispose();
+
+                    GC.Collect();
+                    GC.WaitForPendingFinalizers();
+                    GC.Collect();
+                }
+
+                _fileStream = File.OpenRead(imageName);
+                bitImage.CacheOption = BitmapCacheOption.OnLoad;
                 bitImage.BeginInit();
-                bitImage.UriSource = new Uri(imageName, UriKind.Absolute);
-                bitImage.DecodePixelWidth = (int)(clientWidth);  
-                bitImage.DecodePixelHeight = (int)(clientHeight - MainMenuBar.RenderSize.Height);
+                bitImage.StreamSource = _fileStream;
+                //bitImage.DecodePixelWidth = (int)(clientWidth);  
+                //bitImage.DecodePixelHeight = (int)(clientHeight - MainMenuBar.RenderSize.Height);
                 bitImage.EndInit();
                 bitImage.Freeze();
+                SlideImage.Source = bitImage;
+                
 
-                MainStackPanel.Height = clientHeight;
-                MainStackPanel.Width = clientWidth;
-                MainStackPanel.Height = double.NaN;
-                MainStackPanel.Width = double.NaN;
+                //MainStackPanel.Height = clientHeight;
+                //MainStackPanel.Width = clientWidth;
+
 
                 if (_updateFirstImage)
                 {
                     //SlideImage.Width = (int)clientWidth;
                     //SlideImage.Height = (int)clientHeight;
+                    //SlideImage.Source = bitImage;
+                    //_updateFirstImage = false;
 
-                    SlideImage.Source = bitImage;
-                    _updateFirstImage = false;
-                    SlideImage.BeginAnimation(Image.OpacityProperty, _fadeInAnimation);
-                    SlideImageSecond.BeginAnimation(Image.OpacityProperty, _fadeOutAnimation);
+                    //_fadeOutAnimation.Completed += (o, e) =>
+                    //{
+                    //    SlideImage.Source = bitImage;
+                    //    SlideImage.BeginAnimation(Image.OpacityProperty, _fadeInAnimation);
+                    //};
+                    //SlideImage.BeginAnimation(Image.OpacityProperty, _fadeOutAnimation);
+                    //SlideImageSecond.BeginAnimation(Image.OpacityProperty, _fadeOutAnimation);
                 }
                 else
                 {
                     //SlideImageSecond.Width = (int)clientWidth;
                     //SlideImageSecond.Height = (int)clientHeight;
 
-                    SlideImageSecond.Source = bitImage;
-                    _updateFirstImage = true;
-                    SlideImage.BeginAnimation(Image.OpacityProperty, _fadeOutAnimation);
-                    SlideImageSecond.BeginAnimation(Image.OpacityProperty, _fadeInAnimation);
+                    //SlideImageSecond.Source = bitImage;
+                    //_updateFirstImage = true;
+                    //SlideImage.BeginAnimation(Image.OpacityProperty, _fadeOutAnimation);
+                    //SlideImageSecond.BeginAnimation(Image.OpacityProperty, _fadeInAnimation);
                 }
+                //MainStackPanel.Height = double.NaN;
+                //MainStackPanel.Width = double.NaN;
+                //GC.Collect();
             }
             catch { }
         }
@@ -146,7 +167,7 @@ namespace SuperSliderWin
                 return;
             }
 
-            _imageFiles = Directory.GetFiles(this.Settings.Folders, "*.jpg", SearchOption.AllDirectories);
+            _imageFiles = Directory.GetFiles(this.Settings.Folders, "*.*", SearchOption.AllDirectories);
         }
 
         private void ShowRandomImageMenu_Click(object sender, RoutedEventArgs e)
@@ -158,6 +179,29 @@ namespace SuperSliderWin
         private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void FullScreenMenu_Click(object sender, RoutedEventArgs e)
+        {
+            MenuItem menu = (MenuItem)sender;
+            if (menu.Header.ToString() == "[ ]")
+            {
+                this.WindowStyle = WindowStyle.None;
+                this.ResizeMode = ResizeMode.NoResize;
+                this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+                this.WindowState = WindowState.Maximized;
+
+                menu.Header = "=";
+            }
+            else
+            {
+                this.WindowStyle = WindowStyle.SingleBorderWindow;
+                this.ResizeMode = ResizeMode.CanResize;
+                //this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+                this.WindowState = WindowState.Normal;
+
+                menu.Header = "[ ]";
+            }
         }
     }
 }
